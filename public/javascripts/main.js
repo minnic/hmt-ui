a709762df1a15a7e78ece2e02519aba25d9712b5
jchencc <jarvis_chen@yeah.net>
2015-03-09 11:22:03 +0800

    Highlight current tab in sidebar

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,13 +5,66 @@ hmt.config(function($routeProvider) {
         templateUrl: '/home',
         controller: 'HomeController'
     }).when('/history', {
-        templateUrl: '/history'
+        templateUrl: '/history',
+        controller: 'HistoryController'
     }).when('/queue', {
-        templateUrl: '/queue'
+        templateUrl: '/queue',
+        controller: 'QueueController'
     }).otherwise('/home');
+
 });
 
-hmt.controller('HomeController', ['$scope', function($scope) {
+hmt.factory('TabFactory', function() {
+    var tabs = [
+        {
+            href: '#/home',
+            iconClass: 'glyphicon glyphicon-home',
+            label: 'Home'
+        },
+        {
+            href: '#/history',
+            iconClass: 'glyphicon glyphicon-time',
+            label: 'History'
+        },
+        {
+            href: '#/queue',
+            iconClass: 'glyphicon glyphicon-list-alt',
+            label: 'Queue'
+        }
+    ],
+    factory = {};
+
+    var currentTab = '#/home';
+
+    factory.setCurrentTab = function(tab) {
+        currentTab = tab;
+    };
+
+    factory.getCurrentTab = function() {
+        return currentTab;
+    };
+
+    factory.getTabs = function() {
+        return tabs;
+    };
+
+    return factory;
+
+});
+
+hmt.controller('MainController', ['$scope', 'TabFactory', function($scope, tabFactory) {
+    $scope.tabs = tabFactory.getTabs();
+
+    $scope.isCurrentTab = function(tab) {
+        return tabFactory.getCurrentTab() == tab;
+    };
+
+}]);
+
+hmt.controller('HomeController', ['$scope', 'TabFactory', function($scope, tabFactory) {
+
+    tabFactory.setCurrentTab('#/home');
+
     $scope.srcPoolOnFocus = function() {
         console.log('focus');
     };
@@ -23,4 +76,12 @@ hmt.controller('HomeController', ['$scope', function($scope) {
     $scope.srcPoolOnBlur = function() {
         console.log('blur');
     };
+}]);
+
+hmt.controller('HistoryController', ['$scope', 'TabFactory', function($scope, tabFactory) {
+    tabFactory.setCurrentTab('#/history');
+}]);
+
+hmt.controller('QueueController', ['$scope', 'TabFactory', function($scope, tabFactory) {
+    tabFactory.setCurrentTab('#/queue');
 }]);
